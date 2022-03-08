9412cb71b1bbcd4331dfbafc249557ebe4c48545
Staskevic <83286755+Staskevic@users.noreply.github.com>
2022-03-08 23:44:15 +0000

    Update scrollani2.js

--- scrollani2.js
+++ scrollani2.js
@@ -1,43 +1,50 @@
-const html = document.documentElement;
-const canvas = document.getElementById("hero-lightpass");
-const context = canvas.getContext("2d");
+        // Canvas settings
+        const canvas = document.getElementById("hero-lightpass");
+        const context = canvas.getContext("2d");
 
-const frameCount = 418;
-const currentFrame = index => (
-  `/webp2/mizgl-00${index.toString().padStart(3, '0')}.webp`
-)
+        canvas.width=520;
+        canvas.height=770;
 
-const preloadImages = () => {
-  for (let i = 1; i < frameCount; i++) {
-    const img = new Image();
-    img.src = currentFrame(i);
-  }
-};
+        
+        // Preloading images to drastically improve performance
+        const currentFrame = index => (`/image/mizgl-00${index.toString().padStart(3, '0')}.webp`);
+        const frameCount = 417; // There 148 images for that animation-sequence to load
+        const images = [];
 
-const img = new Image()
-img.src = currentFrame(1);
-// canvas.width=1158;
-canvas.width=520;
-canvas.height=770;
-img.onload=function(){
-  context.drawImage(img, 0, 0);
-}
+        const preloadImages = () => {
+            for (let i = 1; i < frameCount; i++) {
+                images[i] = new Image(); // This is functionally equivalent to document.createElement('img').
+                images[i].src = currentFrame(i);
+            }
+        };
 
-const updateImage = index => {
-  img.src = currentFrame(index);
-  context.drawImage(img, 0, 0);
-}
+        preloadImages();
 
-window.addEventListener('scroll', () => {  
-  const scrollTop = html.scrollTop;
-  const maxScrollTop = html.scrollHeight - window.innerHeight;
-  const scrollFraction = scrollTop / maxScrollTop;
-  const frameIndex = Math.min(
-    frameCount - 1,
-    Math.ceil(scrollFraction * frameCount)
-  );
-  
-  requestAnimationFrame(() => updateImage(frameIndex + 1))
-});
 
-preloadImages()
+        // Draw the first image
+        const img = new Image();
+        img.src = currentFrame(1);
+        img.onload = function(){
+            context.drawImage(img, 0, 0);
+        }
+
+
+        // Scroll interactions
+        const html = document.getElementsByTagName('html');
+        
+        window.addEventListener('scroll', () => {  
+            const scrollTop = html[0].scrollTop;
+            // console.log('scrollTop: ', scrollTop);
+            // console.log('html.scrollHeight: ', html[0].scrollHeight);
+            // console.log('window.innerHeight: ', window.innerHeight);
+            const maxScrollTop = html[0].scrollHeight - window.innerHeight;
+            const scrollFraction = scrollTop / maxScrollTop;
+            const frameIndex = Math.min(
+                frameCount - 1,
+                Math.floor(scrollFraction * frameCount)
+            );
+            // console.log('FrameIndex', frameIndex);
+
+            requestAnimationFrame(() => context.drawImage(images[frameIndex + 1], 0, 0));
+
+        });
